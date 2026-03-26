81fb1cb43fbf8b8daf7a2d85232dd0dfa03d0d87
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-03-25 22:07:19 -0500

    Se hizo un cambio en el main

--- Main.hs
+++ Main.hs
@@ -18,68 +18,89 @@ main = do
 menuPrincipal :: Materia -> IO ()
 menuPrincipal mat = do
     putStrLn "\nMENU PRINCIPAL"
-    putStrLn "1. Gestion de Estudiantes (Agregar)"
-    putStrLn "2. Ver Reportes"
-    putStrLn "3. Ver Ranking Academico"
-    putStrLn "4. Salir"
+    putStrLn "1. Agregar estudiante"
+    putStrLn "2. Agregar calificacion a estudiante"
+    putStrLn "3. Ver reportes"
+    putStrLn "4. Ver ranking"
+    putStrLn "5. Salir"
     putStr "\nSeleccione una opcion: "
     
     opcion <- getLine
 
     case opcion of
 
-        --  AGREGAR ESTUDIANTE 
+        --  AGREGAR ESTUDIANTE
         "1" -> do
-            putStrLn "Ingrese codigo del estudiante:"
+            putStrLn "Ingrese codigo:"
             cod <- getLine
 
-            putStrLn "Ingrese nombre del estudiante:"
+            putStrLn "Ingrese nombre:"
             nombre <- getLine
 
-            
             let nuevo = Estudiante cod nombre [] []
 
             let existe = any (\e -> codigo e == cod) (estudiantes mat)
 
             if existe then do
-                putStrLn "Error: ya existe un estudiante con ese codigo"
+                putStrLn "Error: codigo duplicado"
                 menuPrincipal mat
             else do
                 let nuevaMateria = mat { estudiantes = nuevo : estudiantes mat }
-                putStrLn "Estudiante agregado correctamente"
+                putStrLn "Estudiante agregado"
                 menuPrincipal nuevaMateria
 
 
-        -- REPORTES
+        -- AGREGAR CALIFICACION
         "2" -> do
-            putStrLn "\nREPORTE DE MATERIA"
+            putStrLn "Ingrese codigo del estudiante:"
+            cod <- getLine
+
+            putStrLn "Ingrese nota:"
+            notaStr <- getLine
+            let nota = read notaStr :: Double
+
+            let nuevaLista = map (agregarNotaAEstudiante cod nota) (estudiantes mat)
+
+            let nuevaMateria = mat { estudiantes = nuevaLista }
+
+            putStrLn "Proceso completado"
+            menuPrincipal nuevaMateria
+
+
+        -- REPORTES
+        "3" -> do
             putStrLn (reporteMateria mat)
 
             putStrLn "\nPromedio de la materia:"
             print (promedioMateria mat)
 
-            putStrLn "\nEstudiantes aprobados:"
-            print (nombresAprobados mat)
-
-            putStrLn "\nTabla de promedios:"
-            print (tablaPromedios mat)
-
             menuPrincipal mat
 
 
         -- RANKING
-        "3" -> do
-            let rank = rankingEstudiantes mat
-            putStrLn "\nRANKING DE ESTUDIANTES (Mayor a menor)"
-            print rank
+        "4" -> do
+            print (rankingEstudiantes mat)
             menuPrincipal mat
 
 
         -- SALIR
-        "4" -> putStrLn "Saliendo del sistema..."
+        "5" -> putStrLn "Saliendo..."
 
 
-        -- ERROR
         _ -> do
-            putStrLn "Opcion no valida."
+            putStrLn "Opcion invalida"
             menuPrincipal mat
+
+
+
+-- usa Either y historial
+agregarNotaAEstudiante :: String -> Double -> Estudiante -> Estudiante
+agregarNotaAEstudiante cod nota est
+    | codigo est /= cod = est
+    | otherwise =
+        case agregarCalificacion nota est of
+            Left err -> est  -- no cambia si hay error
+            Right nuevoEst ->
+                nuevoEst {
+                    historial = historial est ++ [AgregarNota nota]
+                }
