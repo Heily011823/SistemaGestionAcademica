f35ed5c2148f00e7c84083908bb049a6a6dc99ea
Heily Rios <heilyy.riosa@autonoma.edu.co>
2026-03-26 09:04:01 -0500

    Se subio arhivo que no se habia subido

--- Reportes.hs
+++ Reportes.hs
@@ -1,5 +1,7 @@
 module Reportes where
+
 import Tipos
+import FuncionesBasicas (mejorNota, peorNota)
 
 -- Parte 5: REPORTES
 
@@ -8,12 +10,17 @@ promedioMaybe :: [Double] -> Maybe Double
 promedioMaybe [] = Nothing
 promedioMaybe xs = Just (sum xs / fromIntegral (length xs))
 
--- Mostrar promedio 
+-- Mostrar promedio desde lista
 mostrarPromedio :: [Double] -> String
 mostrarPromedio xs =
     case promedioMaybe xs of
         Nothing -> "Sin notas"
         Just p  -> show p
+
+-- Mostrar Maybe
+mostrarMaybe :: Maybe Double -> String
+mostrarMaybe Nothing  = "Sin notas"
+mostrarMaybe (Just x) = show x
 
 -- Estado del estudiante
 estadoEstudiante :: Estudiante -> String
@@ -29,9 +36,11 @@ reporteEstudiante est =
     "Codigo: " ++ codigo est ++ "\n" ++
     "Nombre: " ++ nombreEst est ++ "\n" ++
     "Calificaciones: " ++ show (califs est) ++ "\n" ++
+    "Cantidad de calificaciones: " ++ show (length (califs est)) ++ "\n" ++
     "Promedio: " ++ mostrarPromedio (califs est) ++ "\n" ++
-    "Estado: " ++ estadoEstudiante est ++ "\n" ++
-    "Historial: " ++ show (historial est) ++ "\n"
+    "Mejor nota: " ++ mostrarMaybe (mejorNota est) ++ "\n" ++
+    "Peor nota: " ++ mostrarMaybe (peorNota est) ++ "\n" ++
+    "Estado: " ++ estadoEstudiante est ++ "\n"
 
 -- REPORTE DE MATERIA 
 reporteMateria :: Materia -> String
